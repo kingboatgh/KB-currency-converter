46e23dbdbb8e470e1b1c061b3736ab8b3928e7da
Kingsley Boateng <geniusboateng@gmail.com>
2024-10-20 22:01:16 +0000

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ConverterForm from "./components/ConverterForm"
+
+const App = () => {
+  return (
+    <div className="kb-currency-converter">
+      <h2 className="converter-title">KB Currency Converter</h2>
+      <ConverterForm />
+    </div>
+  )
+}
+
+export default App
